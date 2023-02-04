558c0a29953dcf7e6ac7d3f780032f5a91ed6912
David <david_zetterdahl@hotmail.com>
2023-02-04 22:44:29 +0100

    Powerup pickups

---
+++ Assets/_Game/Scripts/Gameplay/Powerups/PowerupPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Gameplay.Powerups
+{
+    public class PowerupPickup : MonoBehaviour
+    {
+        [SerializeField] GameObject weaponToPickup;
+
+        [SerializeField] RandomSoundPlay randomSoundPlay;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Something entered a powerup!");
+            randomSoundPlay.PlayRandomSound();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Gameplay/Powerups/PowerupRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Gameplay.Powerups
+{
+    public class PowerupRotation : MonoBehaviour
+    {
+        [SerializeField] float rotationSpeed = 20f;
+        private void Update()
+        {
+            transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
