ec1b7528671b31dec5dab921f99b89cfa334a45c
David <david_zetterdahl@hotmail.com>
2023-02-05 11:26:16 +0100

    Added the cannonBow weapon tho it's not much like a bow

---
+++ Assets/_Game/Scripts/Gameplay/Missiles/MissileSpawnMultiple.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Effects
+{
+    public class MissileSpawnMultiple : MonoBehaviour
+    {
+        [SerializeField] Rigidbody[] listBodies;
+        [SerializeField] float firingForceRight, firingForceUp;
+
+        private void Start()
+        {
+            foreach (Rigidbody body in listBodies)
+            {
+                body.AddForce(body.transform.right * firingForceRight);
+                body.AddForce(body.transform.up * firingForceUp);
+            }
+        }
+    }
+}
